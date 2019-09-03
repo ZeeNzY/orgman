e0aa52055f9bb7032ecbcf06a9f498795ba6ef59
ZeeNzY <zinzigulwa@gmail.com>
2019-09-03 17:51:34 +0200

    committed

--- src/test/java/ac/za/cput/adp3/xyzcongolmerate/factory/demography/GenderFactoryTest.java
+++ src/test/java/ac/za/cput/adp3/xyzcongolmerate/factory/demography/GenderFactoryTest.java
@@ -1,21 +1,25 @@
 package ac.za.cput.adp3.xyzcongolmerate.factory.demography;
 
+import ac.za.cput.adp3.xyzcongolmerate.domain.demography.Gender;
+import ac.za.cput.adp3.xyzcongolmerate.domain.user.User;
+import org.junit.Assert;
 import org.junit.Test;
+
+import static jdk.dynalink.linker.support.Guards.isNotNull;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GenderFactoryTest {
 
-    //TODO: implement method body ONLY!
+    private GenderFactory genderFactory;
+    private Gender gender;
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
     @Test
     public void buildGender() {
-        throw new UnsupportedOperationException("Not supported yet!");
-        /**
-         * Your implementation goes here
-         *
-         * INSTRUCTION
-         * 1. Remove line [//TODO: implement method body ONLY!]
-         * 2. Remove line [throw new UnsupportedOperationException("Not yet supported!");]
-         * 3. Test the GenderFactory class
-         * 4. Assert that the id is generated.
-         */
     }
+
 }
